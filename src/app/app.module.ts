9f6964e2a4ce8662e550bea24b78f810f1dc1092
Ben Lang <bl3rune@github.com>
2018-01-07 15:59:09 +0000

    first push

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { ButtonComponent } from './core/button/button.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ButtonComponent
   ],
   imports: [
     BrowserModule

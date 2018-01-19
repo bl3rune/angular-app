1d4c042d79b8c97d11cd7f2fe80be6f48b7ff779
Ben Lang <bl3rune@github.com>
2018-01-19 22:19:11 +0000

    nav

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
 import { ButtonComponent } from './core/button/button.component';
+import { NavbarComponent } from './core/navbar/navbar.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    ButtonComponent
+    ButtonComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule

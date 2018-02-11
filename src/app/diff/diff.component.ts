c920755233662320e90424b3c6bd55988c403822
Ben Lang <bl3rune@github.com>
2018-02-11 13:10:49 +0000

    started diff and logo for sort done

--- src/app/diff/diff.component.ts
+++ src/app/diff/diff.component.ts
@@ -5,11 +5,17 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './diff.component.html',
   styleUrls: ['./diff.component.css']
 })
-export class DiffComponent implements OnInit {
+export class DiffComponent {
+
+  output: string;
 
   constructor() { }
 
-  ngOnInit() {
+  sort(a: string, b: string) {
+    if (a && b) {
+      const left = a.split('\n');
+      const right = b.split('\n');
+    }
   }
 
 }
--- src/app/sort/sort.component.ts
+++ src/app/sort/sort.component.ts
@@ -16,7 +16,6 @@ export class SortComponent {
   }
 
   sortable(a: string, b: string): number {
-    console.log(a.toLowerCase() > b.toLowerCase() ? 1 : (a.toLowerCase() === b.toLowerCase() ? 0 : -1));
     return (a.toLowerCase() > b.toLowerCase() ? 1 : (a.toLowerCase() === b.toLowerCase() ? 0 : -1));
   }
 

ff70f4cdf4ccf2f0e6906ac436d6c73b5acdf300
Ben Lang <ben.lang@nakedwines.com>
2019-06-03 17:36:03 +0100

    diff viewer working

--- src/app/diff/diff.component.ts
+++ src/app/diff/diff.component.ts
@@ -15,6 +15,30 @@ export class DiffComponent {
     if (a && b) {
       const left = a.split('\n');
       const right = b.split('\n');
+      const diffs = [];
+      let rightIndex = 0;
+      left.forEach(row => {
+        for (let i = rightIndex.valueOf(); i < right.length; i++) {
+          if (right[i] === row) {
+            if (i !== rightIndex.valueOf()) {
+              for (let j = rightIndex.valueOf(); j < i; j++) {
+                diffs.push('+:: ' + right[j]);
+                console.log('added');
+              }
+            }
+            diffs.push('=:: ' + right[i]);
+            rightIndex = i + 1;
+            return;
+          }
+        }
+        diffs.push('-:: ' + row);
+        console.log('removed');
+      });
+      for (let i = rightIndex.valueOf(); i < right.length; i++) {
+        diffs.push('+:: ' + right[i]);
+        console.log('added');
+      }
+      this.output = diffs.join('\n');
     }
   }
 

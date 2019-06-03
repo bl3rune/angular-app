bdf12d8d97f2dc209f81325194b8d3c4d4118afc
Ben Lang <ben.lang@nakedwines.com>
2019-06-03 16:19:11 +0100

    fixed timestamp converter

--- src/app/converter/converter.component.ts
+++ src/app/converter/converter.component.ts
@@ -9,7 +9,6 @@ export class ConverterComponent {
 
   now: number = Date.now();
   date: Date;
-  date2: Date;
   epoch: number;
 
   constructor() { }
@@ -17,11 +16,14 @@ export class ConverterComponent {
   epochMSecondsToDate(input: number) {
     const d = new Date(0);
     d.setUTCMilliseconds(input);
+    d.setUTCHours(0);
+    d.setUTCMinutes(0);
+    d.setUTCSeconds(0);
     this.date = d;
   }
 
-  dateToEpoch(input: string) {
-    this.epoch = new Date(input).getUTCSeconds();
+  dateToEpoch(input: Date) {
+    this.epoch = input.valueOf();
   }
 
 }
